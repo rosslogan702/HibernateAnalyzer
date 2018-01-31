a9064b6e255b244dcc46a290c999ab6682d920d3
rosslogan702 <rosslogan702@gmail.com>
2018-01-31 22:12:50 +0000

    Adding feature to find the maxima and minima of each indicator (internet users and adult literacy rate)

--- src/main/java/com/rosslogan/analyzer/Application.java
+++ src/main/java/com/rosslogan/analyzer/Application.java
@@ -8,7 +8,9 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Ross on 29/01/2018.
@@ -31,6 +33,36 @@ public class Application {
         System.out.printf(RESULTS_HEADER);
         System.out.printf(COLUMNS_RESULTS_SEPARATOR);
         fetchAllData().stream().forEach(System.out::println);
+
+        List<Country> countryData = fetchAllData();
+
+        Comparator<Country> internetUsers = Comparator.comparingDouble(Country::getInternetUsers);
+        Comparator<Country> adultLiteracyRate = Comparator.comparingDouble(Country::getAdultLiteracyRate);
+
+        List<Country> validInternetUsers = countryData.stream().filter(country ->
+                country.getInternetUsers()!=null).collect(Collectors.toList());
+        List<Country> validAdultLiteracy = countryData.stream().filter(country ->
+                country.getAdultLiteracyRate()!=null).collect(Collectors.toList());
+
+
+        //Find min and max internet users
+        Country minInternetUsers = validInternetUsers.stream().min(internetUsers).get();
+        Country maxInternetUsers = validInternetUsers.stream().max(internetUsers).get();
+
+        //Find min and max adult literacy rate
+        Country minAdultLiteracyRate = validAdultLiteracy.stream().min(adultLiteracyRate).get();
+        Country maxAdultLiteracyRate = validAdultLiteracy.stream().max(adultLiteracyRate).get();
+
+        System.out.printf("%n%nStatistics Output%n%n");
+        System.out.println("Min Internet Users: " + minInternetUsers.getName() +
+                " " + minInternetUsers.getInternetUsers());
+        System.out.println("Max Internet users: " + maxInternetUsers.getName() +
+                " " + maxInternetUsers.getInternetUsers());
+        System.out.println("Min Adult Literacy: " + minAdultLiteracyRate.getName() +
+                " " + minAdultLiteracyRate.getAdultLiteracyRate());
+        System.out.println("Max Adult Literacy: " + maxAdultLiteracyRate.getName() +
+                " " + maxAdultLiteracyRate.getAdultLiteracyRate());
+
     }
 
     @SuppressWarnings("unchecked")
@@ -49,4 +81,5 @@ public class Application {
 
         return contacts;
     }
+
 }
